1c8acb5f4c8494e91ef557ee3b79539240ab0f14
Andres Lujan <andreslujandev@gmail.com>
2020-03-26 23:04:38 -0500

    Add CORS support

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask import Flask
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 app.debug = True
 app.config['host'] = '0.0.0.0'
 
